1385a173be3f98bc2104949b1f1da420d588659b
phobicadz <chamberlain_adam@hotmail.com>
2016-04-14 15:14:03 +0100

    First commit with bower controls

--- NodeLed/app.js
+++ NodeLed/app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
--- NodeLed/public/javascripts/AngularApp.js
+++ NodeLed/public/javascripts/AngularApp.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('NodeLed', []);
+﻿var app = angular.module('NodeLed', ['ngRoute']);
 
 // should define this in seperate file as each controller should have one
 app.controller('myCtrl', function ($scope) {
