60e0f6300c9f3e32e4bb4867b06bbb804b85a979
phobicadz <chamberlain_adam@hotmail.com>
2016-04-20 14:34:28 +0100

    more work done lol

--- NodeLed/public/javascripts/ledController.js
+++ NodeLed/public/javascripts/ledController.js
@@ -10,16 +10,52 @@ nodeledApp.controller('ledController', function ($scope) {
     for (a=0;a < numRows;a++)
     {
         var data = [];
+        var oddIndex = ledNumber + (numCols-1);
         for(b=0;b < numCols; b++)
         {
-            data.push({
-                "id": ledNumber, "rgb": "#fffff", "yep" : isOdd(a)
-            });
+            if (isOdd(a))
+            {
+                data.push({
+                    "id": oddIndex, "rgb": "#fffff", "yep": isOdd(a)
+                });
+                oddIndex--;
+            }
+            else
+            {
+                data.push({
+                    "id": ledNumber, "rgb": "#fffff", "yep": isOdd(a)
+                });
+            }      
             ledNumber++;
         }
         $scope.leds.push(data);     
     }
 
-    function isOdd(num) { return (num % 2) == 1; }
+    $scope.leds.sort();
+
+    function isOdd(num) {
+        return (num % 2) == 1;
+    }
+
+    $scope.suck = function()
+    {
+        alert($scope.leds[0][0].rgb);
+
+        // sort to so it will be sent in as one string
+        $scope.leds[1].sort(predicateBy("id"));
+
+    }
+
+    // uses predicate function for sorting much like .Net
+    function predicateBy(prop) {
+        return function (a, b) {
+            if (a[prop] > b[prop]) {
+                return 1;
+            } else if (a[prop] < b[prop]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
 
 });
